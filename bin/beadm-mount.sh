cd67da2a51ea6a60fad2b2914589d8a84d341506
Jim Klimov <jim@jimklimov.com>
2015-02-06 12:42:03 +0100

    beadm-upgrade.sh: reorganized code into functions, and in particular allowed to call mount and unmount logic explicitly; also support lofs-mounting and unmounting of active rpool/SHARED/* datasets into ABE

---
+++ bin/beadm-mount.sh
@@ -0,0 +1,1 @@
+beadm-upgrade.sh
--- bin/beadm-upgrade.sh
+++ bin/beadm-upgrade.sh
@@ -4,6 +4,11 @@
 ### Copyright (C) 2014-2015 by Jim Klimov, License: CDDL
 ### See also: http://wiki.openindiana.org/oi/Advanced+-+Split-root+installation
 
+PATH=/usr/gnu/bin:/usr/sfw/bin:/opt/gnu/bin:/opt/sfw/bin:/opt/omni/bin:/bin:/sbin:/usr/sbin:/usr/bin:$PATH
+LANG=C
+LC_ALL=C
+export LANG LC_ALL PATH
+
 [ ! -s "`dirname $0`/beadm-clone.sh" ] && \
 	echo "FATAL: Can't find beadm-clone.sh" >&2 && \
 	exit 1
@@ -13,15 +18,16 @@ RES_PKGSRC=-1
 RES_BOOTADM=-1
 BREAKOUT=n
 
-trap_exit() {
+trap_exit_upgrade() {
     RES_EXIT=$1
 
     echo ""
     beadm list $BENEW
+    /bin/df -k | awk '( $NF ~ "^'"$BENEW_MNT"'($|/)" ) { print $0 }'
     echo ""
 
     if [ $RES_EXIT = 0 -a $BREAKOUT = n -a \
-	 $RES_PKGIPS -le 0 -a $RES_PKGSRC -le 0 -a $RES_BOOTADM -le 0 ] ; then
+        $RES_PKGIPS -le 0 -a $RES_PKGSRC -le 0 -a $RES_BOOTADM -le 0 ] ; then
         echo "=== SUCCESS, you can now do:  beadm activate $BENEW" >&2
         exit 0
     fi
@@ -33,73 +39,174 @@ trap_exit() {
     exit $RES_EXIT
 }
 
-trap "BREAKOUT=y; exit 127;" 1 2 3 15
-trap 'trap_exit $?' 0
+trap_exit_mount() {
+    RES_EXIT=$1
 
-# Support package upgrades in an existing (alt)root
-if [ -n "$BENEW" ] && beadm list "$BENEW" > /dev/null ; then
-    echo "INFO: It seems that BENEW='$BENEW' already exists; ensuring it is mounted..."
-    _BEADM_CLONE_INFORM=no _BEADM_CLONE=no . "`dirname $0`/beadm-clone.sh"
-    RES=$?
+    echo ""
+    beadm list $BENEW
+    /bin/df -k | awk '( $NF ~ "^'"$BENEW_MNT"'($|/)" ) { print $0 }'
+    echo ""
 
-    if [ -n "$BENEW_MNT" -a -n "$BENEW_DS" -a -n "$BENEW" -a $RES = 0 ]; then
-	_MPT="`/bin/df -Fzfs -k "$BENEW_MNT" | awk '{print $1}' | grep "$BENEW_DS"`"
-	if [ x"$_MPT" != x"$BENEW_DS" -o -z "$_MPT" ]; then
-		beadm mount "$BENEW" "$BENEW_MNT" || exit
-		_MPT="`/bin/df -Fzfs -k "$BENEW_MNT" | awk '{print $1}' | grep "$BENEW_DS"`"
-		[ x"$_MPT" != x"$BENEW_DS" -o -z "$_MPT" ] && \
-			echo "FATAL: Can't mount $BENEW at $BENEW_MNT"
-	fi
-    else
-	[ "$RES" = 0 ] && RES=125
+    if [ $RES_EXIT = 0 -a $BREAKOUT = n ]; then
+        echo "=== SUCCESS, (un)mounting operations completed for BE '$BENEW' at '$BENEW_MPT'" >&2
+        exit 0
     fi
-else
-    . "`dirname $0`/beadm-clone.sh"
-    RES=$?
-fi
-[ $RES != 0 -o x"$BENEW" = x ] && \
+
+    [ $RES_EXIT = 0 ] && RES_EXIT=126
+    echo "=== FAILED (un)mounting operations for BE '$BENEW' at '$BENEW_MPT'" >&2
+    exit $RES_EXIT
+}
+
+do_ensure_configs() {
+    RES=0
+    if [ -n "$BENEW" ] && beadm list "$BENEW" > /dev/null ; then
+	echo "INFO: It seems that BENEW='$BENEW' already exists; ensuring it is mounted..."
+	_BEADM_CLONE_INFORM=no _BEADM_CLONE=no . "`dirname $0`/beadm-clone.sh"
+	RES=$?
+    else
+	. "`dirname $0`/beadm-clone.sh"
+	RES=$?
+    fi
+    [ $RES != 0 -o x"$BENEW" = x ] && \
 	echo "FATAL: Failed to use beadm-clone.sh" >&2 && \
 	exit 2
 
-beadm list "$BENEW"
-[ $? != 0 ] && \
+    return 0
+}
+
+do_clone_mount() {
+    # This routine ensures that variables have been set and altroot is mounted
+    # Note that we also support package upgrades in an existing (alt)root
+
+    if [ -n "$BENEW_MNT" -a -n "$BENEW_DS" -a -n "$BENEW" ]; then
+	_MPT="`/bin/df -Fzfs -k "$BENEW_MNT" | awk '{print $1}' | grep "$BENEW_DS"`"
+	if [ x"$_MPT" != x"$BENEW_DS" -o -z "$_MPT" ]; then
+	    beadm mount "$BENEW" "$BENEW_MNT" || exit
+	    _MPT="`/bin/df -Fzfs -k "$BENEW_MNT" | awk '{print $1}' | grep "$BENEW_DS"`"
+	    [ x"$_MPT" != x"$BENEW_DS" -o -z "$_MPT" ] && \
+		echo "FATAL: Can't mount $BENEW at $BENEW_MNT" && \
+		exit 3
+	fi
+    else
+	echo "FATAL: Configuration not determined" >&2
+	exit 2
+    fi
+
+    beadm list "$BENEW"
+    [ $? != 0 ] && \
 	echo "FATAL: Failed to locate the BE '$BENEW'" >&2 && \
 	exit 3
 
+    _MPT="`/bin/df -Fzfs -k "$BENEW_MNT" | awk '($1 == "'"$BENEW_DS"'") {print $1}'`"
+    [ x"$_MPT" != x"$BENEW_DS" -o -z "$_MPT" ] && \
+	echo "FATAL: Could not mount $BENEW at $BENEW_MNT"
 
-echo ""
-echo "======================== `date` ==================="
-echo "=== Beginning package upgrades in the '$BENEW' image mounted at '$BENEW_MNT'..."
+    # Now, ensure that shared sub-datasets (if any) are also lofs-mounted
+    for _SMT in /tmp \
+	`/bin/df -k | awk '( $1 ~ "^'"$RPOOL_SHARED"'" ) { print $NF }'` \
+    ; do
+	echo "===== lofs-mount '$_SMT' at '$BENEW_MNT$_SMT'"
+	mount -F lofs -o rw "$_SMT" "$BENEW_MNT$_SMT"
+    done
 
-### Note that IPS pkg command does not work under a simple chroot
-if [ -x /usr/bin/pkg ]; then
-    echo "=== Run IPS pkg upgrade..."
-    /usr/bin/pkg -R "$BENEW_MNT" image-update --accept --licenses --deny-new-be --no-backup-be
-    RES_PKGIPS=$?
-    TS="`date -u "+%Y%m%dZ%H%M%S"`" && \
-	zfs snapshot -r "$RPOOL_SHARED@postupgrade_pkgips-$TS" && \
-	zfs snapshot -r "$BENEW_DS@postupgrade_pkgips-$TS"
-fi
 
-if [ -x "$BENEW_MNT"/opt/local/bin/pkgin ]; then
-    echo "=== Run PKGSRC upgrade..."
-    yes | chroot "$BENEW_MNT" /opt/local/bin/pkgin full-upgrade
-    RES_PKGSRC=$?
-    TS="`date -u "+%Y%m%dZ%H%M%S"`" && \
-	zfs snapshot -r "$RPOOL_SHARED@postupgrade_pkgsrc-$TS" && \
-	zfs snapshot -r "$BENEW_DS@postupgrade_pkgsrc-$TS"
-fi
+    return 0
+}
 
-echo "=== Reconfiguring boot-archive in new BE..."
-touch "$BENEW_MNT/reconfigure" && \
-bootadm update-archive -R "$BENEW_MNT"
-RES_BOOTADM=$?
+do_clone_umount() {
+    # This routine ensures altroot is unmounted
 
-echo "=== Unmounting $BENEW ($BENEW_MNT)..."
-beadm umount "$BENEW_MNT"
+    echo "=== Unmounting BE $BENEW under '$BENEW_MNT'..."
 
-echo "=== Done: IPS=$RES_PKGIPS PKGSRC=$RES_PKGSRC BOOTADM=$RES_BOOTADM"
-echo "=== If upgrade was acceptable and successful:"
-echo ":; beadm activate '$BENEW'"
-echo "=== If you change your mind or if it failed:"
-echo ":; beadm destroy -Ffsv '$BENEW'"
+    /bin/df -k -F lofs | \
+	awk '( $NF ~ "^'"$BENEW_MNT"'/.*" ) { print $1" "$NF }' | \
+	sort -r | while read _LFS _LMT; do
+	    echo "===== unmounting '$_LFS' bound over '$_LMT'..."
+	    umount "$_LMT"
+	done
+
+    echo "===== beadm-unmounting $BENEW ($BENEW_MNT)..."
+    beadm umount "$BENEW_MNT"
+}
+
+do_upgrade_pkgips() {
+    ### Note that IPS pkg command does not work under a simple chroot
+    if [ -x /usr/bin/pkg ]; then
+	echo "=== Run IPS pkg upgrade..."
+	/usr/bin/pkg -R "$BENEW_MNT" image-update --accept --licenses --deny-new-be --no-backup-be
+	RES_PKGIPS=$?
+	TS="`date -u "+%Y%m%dZ%H%M%S"`" && \
+	    zfs snapshot -r "$RPOOL_SHARED@postupgrade_pkgips-$TS" && \
+	    zfs snapshot -r "$BENEW_DS@postupgrade_pkgips-$TS"
+    fi
+}
+
+do_upgrade_pkgsrc() {
+    if [ -x "$BENEW_MNT"/opt/local/bin/pkgin ]; then
+	echo "=== Run PKGSRC upgrade..."
+	yes | chroot "$BENEW_MNT" /opt/local/bin/pkgin full-upgrade
+	RES_PKGSRC=$?
+	TS="`date -u "+%Y%m%dZ%H%M%S"`" && \
+	    zfs snapshot -r "$RPOOL_SHARED@postupgrade_pkgsrc-$TS" && \
+	    zfs snapshot -r "$BENEW_DS@postupgrade_pkgsrc-$TS"
+    fi
+}
+
+do_reconfig() {
+    echo "=== Reconfiguring boot-archive in new BE..."
+    touch "$BENEW_MNT/reconfigure" && \
+	bootadm update-archive -R "$BENEW_MNT"
+    RES_BOOTADM=$?
+}
+
+do_upgrade() {
+    do_ensure_configs || exit
+    do_clone_mount || exit
+
+    echo ""
+    echo ""
+    echo "======================== `date` ==================="
+    echo "=== Beginning package upgrades in the '$BENEW' image mounted at '$BENEW_MNT'..."
+
+    echo ""
+    do_upgrade_pkgips
+
+    echo ""
+    do_upgrade_pkgsrc
+
+    echo ""
+    do_reconfig
+
+    echo ""
+    do_clone_umount
+
+    echo ""
+    echo "=== Done: IPS=$RES_PKGIPS PKGSRC=$RES_PKGSRC BOOTADM=$RES_BOOTADM"
+    echo "=== If upgrade was acceptable and successful:"
+    echo ":; beadm activate '$BENEW'"
+    echo "=== If you change your mind or if it failed:"
+    echo ":; beadm destroy -Ffsv '$BENEW'"
+}
+
+trap "BREAKOUT=y; exit 127;" 1 2 3 15
+
+
+case "`basename $0`" in
+    *upgrade*)
+		trap 'trap_exit_upgrade $?' 0
+		do_upgrade ;;
+    *umount*)	[ -z "$BENEW" ] && \
+		    echo "FATAL: BENEW not defined, nothing to unmount" && \
+		    exit 1
+		trap 'trap_exit_mount $?' 0
+		# As of now, this "curiously" tries to mount the BE first... ;)
+		do_ensure_configs
+		do_clone_umount ;;
+    *mount*)	[ -z "$BENEW" ] && \
+		    echo "FATAL: BENEW not defined, nothing to mount" && \
+		    exit 1
+		trap 'trap_exit_mount $?' 0
+		do_ensure_configs || exit
+		do_clone_mount ;;
+    *)		echo "FATAL: Command not determined"; exit 1 ;;
+esac
